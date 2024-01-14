778877d2c5f6a6e85b2616f9c15aef296d99e637
windmac2002 <macquangphong2002@gmail.com>
2024-01-14 19:20:09 +0700

    btTuan5.2
    day dl tu server len ung dung flutter

---
+++ pr2/lib/main.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+class Product{
+    String search_image;
+    String styleid;
+    String brands_filter_facet;
+    String price;
+    String product_additional_info;
+
+    Product(
+    {
+        required this.search_image,
+        required this.styleid,
+        required this.brands_filter_facet,
+        required this.price,
+         required this.product_additional_info
+    });
+}
+//activity
+class ProductListScreen extends StatefulWidget{
+  @override
+  _ProductListScreenState createState() => _ProductListScreenState();
+
+}
+//b3 difine activity: lay du lieu tu server ve
+class _ProductListScreenState extends State<ProductListScreen> {
+  late List<Product> products;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    products = [];
+    fetchProducts();
+  }
+
+
+  Future<void> fetchProducts() async {
+    final respone = await http.get(
+        Uri.parse('http://192.168.1.192/asever/get.php'));
+    if (respone.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(respone.body);
+      setState(() {
+        products = convertMapToList(data);
+      });
+    }
+    else {
+      throw Exception("khong doc duoc du lieu");
+    }
+  }
+
+  //ham convert map thanh list
+  List<Product> convertMapToList(Map<String, dynamic> data) {
+    List<Product> productList = [];
+    data.forEach((key, value) {
+      for (int i = 0; i < value.length; i++) {
+        Product product = Product(
+            search_image: value[i]['search_image'] ?? '',
+            styleid: value[i]['styleid'] ?? 0,
+            brands_filter_facet: value[i]['brands_filter_facet'] ?? '',
+            price: value[i]['price'] ?? 0,
+            product_additional_info: value[i]['product_additional_info)'] ??
+                '');
+        productList.add(product);
+      }
+    });
+    return productList;
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Danh sach san pham"),
+        ),
+        body: products != null ?
+        ListView.builder(
+            itemCount: products.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(products[index].brands_filter_facet),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Price: ${products[index].price}'),
+                    Text('product_additional_info: ${products[index]
+                        .product_additional_info}'),
+                  ],
+                ),
+                leading: Image.network(
+                  products[index].search_image,
+                  width: 50,
+                  height: 50,
+                  fit: BoxFit.cover,),
+              );
+            })
+            : Center(child: CircularProgressIndicator(),)
+    );
+  }
+}
+  void main() {
+    runApp(const MyApp());
+  }
+
+  class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+  return MaterialApp(
+  title: 'Danh sach san pham',
+  theme: ThemeData(
+
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  useMaterial3: true,
+  ),
+  home: ProductListScreen(),
+  );
+  }
+  }
+
+
+
+
